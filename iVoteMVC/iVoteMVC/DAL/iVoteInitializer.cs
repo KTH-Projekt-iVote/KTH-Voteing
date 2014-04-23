c9af543406abff5dbc922c7c4d9b57b9691eb7d7
sommardaniel <daniel.vaknine@gmail.com>
2014-04-23 16:17:42 +0200

    La till iVoteIntializer

---
+++ iVoteMVC/iVoteMVC/DAL/iVoteInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using iVoteMVC.Models;
+using System.Data.Entity;
+
+namespace iVoteMVC.DAL
+{
+    public class iVoteInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<iVoteContext>
+    {
+        protected override void Seed(iVoteContext context)
+        {
+           
+        }
+    }
+}
