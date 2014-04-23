5ac45841c2f674c58072faeed9c56d6f5fc9dc0a
sommardaniel <daniel.vaknine@gmail.com>
2014-04-23 14:48:28 +0200

    Skapat nytt Projekt
    Nytt projekt,
    Web Asp.net
    MVC 5
    .Net Framework 4.5

---
+++ iVoteMVC/iVoteMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="iVoteMVC.MvcApplication" Language="C#" %>
---
+++ iVoteMVC/iVoteMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iVoteMVC.Startup))]
+namespace iVoteMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
