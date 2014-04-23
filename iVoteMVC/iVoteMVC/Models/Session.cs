161672a8222b93b156bb4e058740ac053cdd63bd
sommardaniel <daniel.vaknine@gmail.com>
2014-04-23 16:00:21 +0200

    Session.cs filen som inte följde med

---
+++ iVoteMVC/iVoteMVC/Models/Session.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iVoteMVC.Models
+{
+    public class Session
+    {
+        public int ID { get; set; }
+        public int TeacherID { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public DateTime dateCreated { get; set; }
+        public DateTime dateModifed { get; set; }
+        public bool published { get; set; }
+
+        public ICollection<Question> questions { get; set; }    
+    }
+}
