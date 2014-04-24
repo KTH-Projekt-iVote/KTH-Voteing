bbf6b626357ea1306722e8934efebab20386e6c6
DjStefano <Mrstefano@hotmail.se>
2014-04-24 15:36:21 +0200

    Ändrat titel på teacher
    Oviktigt, mest för att kunna synca nya händelser.

--- iVoteMVC/iVoteMVC/Models/Session.cs
+++ iVoteMVC/iVoteMVC/Models/Session.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel;
- using System.ComponentModel.DataAnnotations;
- using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace iVoteMVC.Models
 {
